5bd5d2b012a91c061267b525df74728131167e21
Emma Anderson <workeandrs@gmail.com>
2026-08-03 18:40:40 -0700

    updated icon mask

--- app/src/main/java/io/github/draumaz/sustenance/notifications/FastingNotificationWorker.kt
+++ app/src/main/java/io/github/draumaz/sustenance/notifications/FastingNotificationWorker.kt
@@ -30,7 +30,7 @@ class FastingNotificationWorker(
         
         val goalText = if (goal % 1f == 0f) goal.toInt().toString() else goal.toString()
         val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(applicationContext.getString(R.string.fasting_goal_met_title))
             .setContentText(applicationContext.getString(R.string.fasting_goal_met_body, goalText))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
